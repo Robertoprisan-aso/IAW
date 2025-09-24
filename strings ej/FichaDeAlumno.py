59ecc70e425bf28a13f7d199593ed601116a0d0d
OPRISAN <robopr3@ti4-11.infor.caminas>
2025-09-24 13:23:20 +0200

    finalizado

--- strings ej/FichaDeAlumno.py
+++ strings ej/FichaDeAlumno.py
@@ -3,6 +3,7 @@ edad = 21
 curso = '2º ASIR'
 avg_nota = 7.5
 
-print(nombre, edad, curso, sep='|')
+frase = (f"{nombre} \n{edad} \n{curso} \n{avg_nota}")
+print(frase)
 print("Nota media:", avg_nota)
       
--- strings ej/presentacionPersonal.py
+++ strings ej/presentacionPersonal.py
@@ -1,4 +1,4 @@
 nombre = "Robert"
 edad = 21
 
-print('Hola, me llamo ' + nombre + ' y tengo '+ str(edad), ' años')
+print('Hola, me llamo ' + nombre + ' y tengo '+ str(edad), 'años')
